570de39a25b67f1527cc60d54020aff8c629990d
tiamhot12 <tiamhot12@gmail.com>
2019-02-20 08:08:04 -0500

    add styling

--- components/Header.js
+++ components/Header.js
@@ -5,14 +5,50 @@ const linkStyle = {
 }
 
 const Header = () => (
-    <div>
+
+  
+  
+  
+    <div className = "navheader">
+    <style jsx>{`
+.navheader a {
+  float: left;
+  color: black;
+  text-align: center;
+  padding: 12px;
+  text-decoration: none;
+  font-size: 18px; 
+  line-height: 25px;
+  border-radius: 4px;
+}
+
+.navheader a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.navheader a.active {
+  background-color: dodgerblue;
+  color: white;
+}
+
+.navheader{
+  overflow: hidden;
+  background-color: #f1f1f1;
+  padding: 20px 10px;
+
+}
+    `}</style>
+  
         <Link href="/">
-          <a style={linkStyle}>Home</a>
+          <a className="headerbutton">Home</a>
         </Link>
         <Link href="/about">
-          <a style={linkStyle}>About</a>
+          <a className="headerbutton">About</a>
         </Link>
     </div>
+
+
 )
 
 export default Header
