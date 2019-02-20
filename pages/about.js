559ac77d9cdcc37ff8eb3ca3387573e956bfdae8
tiamhot12 <tiamhot12@gmail.com>
2019-02-20 08:08:52 -0500

    add styling

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,8 @@ import Layout from '../components/Layout'
 
 const About = () => (
  <Layout>
-    <p>This is the about page</p>
+    <h1>Alvaro´s personal webpage</h1>
+    <p> Hi, thanks for coming by. I'm using this webpage as a playground to improve my skills as webapp developer. For now I don't have much to show </p>
  </Layout>
 )
 
