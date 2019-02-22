37639d347c435e4acdb8c8850ae87eb3ac54e32c
tiamhot12 <tiamhot12@gmail.com>
2019-02-22 04:16:50 -0500

    refactoring

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,12 @@
 import Header from '../components/Header'
 import Layout from '../components/Layout'
+import Card from '../components/Card'
 
 
 
 const About = () => (
  <Layout>
-    <h1>Alvaro´s personal webpage</h1>
-    <p> Hi, thanks for coming by. I'm using this webpage as a playground to improve my skills as webapp developer. For now I don't have much to show </p>
+   <Card title='Alvaro´s personal webpage' text="Hi, thanks for coming by. I'm using this webpage as a playground to improve my skills as webapp developer. For now I don't have much to show"/>
  </Layout>
 )
 
